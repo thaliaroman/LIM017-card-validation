97d9d2eaad947524861622c5009633a780df62f0
thaliaroman <thaly31955@gmail.com>
2022-02-08 15:03:18 -0500

    pass test

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import validator from './validator.js';
 
 document.getElementById("botonvalidar").addEventListener("click",validator.isValid(document.getElementById("botonvalidar").value));
 
-prompt("Escribe tu nombre");
-function saludo(){
-    document.write("Hola "+ user +" es un placer ayudarte el dia de hoy");
-}
- */
+// // prompt("Escribe tu nombre");
+// function saludo(){
+//     document.write("Hola "+ user +" es un placer ayudarte el dia de hoy");
+// }
+//  */
