bccaa46066c61aa3ae777ebc8364e01588eee309
thaliaroman <thaly31955@gmail.com>
2022-02-08 14:48:08 -0500

    pass test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import validator from './validator.js';
 
-console.log(validator);
+document.getElementById("botonvalidar").addEventListener("click",validator.isValid(document.getElementById("botonvalidar").value));
+
+prompt("Escribe tu nombre");
+function saludo(){
+    document.write("Hola "+ user +" es un placer ayudarte el dia de hoy");
+}
+ */
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,65 @@
+
 const validator = {
-  // ...
-};
+  isValid:function(cn){
+    
+    let cardNumber = cn;
+    let invertedCardNumber = cardNumber.split("").reverse();
+    let convertNumberArray = invertedCardNumber.map(Number);
+    let multBy2 = function(element, index){
+      if (index % 2 !== 0){
+        return element*2;
+      } else {
+        return element;
+      }
+    };
+    let multBy2Array= convertNumberArray.map(multBy2);
+
+    let finalArray=multBy2Array.map(function(element){
+      if (element>=10){
+        return element-9;
+      } else {
+        return element;
+      }
+    });
+    let sumOfNumbers=0;
+    for (let i = 0; i < finalArray.length; i++) {
+    sumOfNumbers += finalArray[i]
+    }
+    let result= function(){
+      if(sumOfNumbers % 10 === 0){
+        return true;
+      } else{
+        return false;
+      }
+      };
+
+    let booleanResult = result();
+    return booleanResult;
+    },
+
+  maskify:function(cn){
+      
+      let cardNumber = cn;
+      let totalDigits=cardNumber.length;
+      if (totalDigits < 4) {return cardNumber;}
+      let indexStart4Digits=totalDigits-4;
+      let digitsToShow=cardNumber.slice(indexStart4Digits,totalDigits);
+      let maskedNumber ='#'.repeat(indexStart4Digits) + digitsToShow;
+      
+      return maskedNumber;
+    }
+ 
+  }
+
+
+
+
+
+
+
+
+
+
+
 
 export default validator;
